2f436cdac8cd0b72f2c5e428021efc04935a07d3
Jorge <jorgeoscargamez@gmail.com>
2018-09-04 01:08:04 -0300

    Solucion de declaracion en Modelo y Vistas

--- resources/views/requisito_postulantes/show_fields.blade.php
+++ resources/views/requisito_postulantes/show_fields.blade.php
@@ -1,12 +1,18 @@
-<!-- Requisitoitem Id Field -->
+
 <div class="form-group">
-    {!! Form::label('requisitoitem_id', 'Requisitoitem Id:') !!}
-    <p>{!! $requisitoPostulante->requisitoitem_id !!}</p>
+    {!! Form::label('concurso_id', 'Datos del Concurso:') !!}
+    <p>
+       Refencia General : {!! $requisitoPostulante->concurso->referenciaGeneral !!} <br>
+       Asignatura : {!! $requisitoPostulante->concurso->asignatura->nombre !!} <br>
+       Area : {!! $requisitoPostulante->concurso->asignatura->area->nombre !!} <br>
+       Estado del Concurso : {!! $requisitoPostulante->concurso->estado !!}
+    </p>
+    <a href="{{ action('ConcursoController@show', $requisitoPostulante->concurso_id ) }}"><p> {!! Form::button('Ver Más!')!!}</a>
 </div>
 
 <!-- Postulante Id Field -->
 <div class="form-group">
-    {!! Form::label('postulante_id', 'Postulante Id:') !!}
+    {!! Form::label('postulante_id', 'Datos del Postulante:') !!}
     <p>
       Identificador : {!! $requisitoPostulante->postulante_id !!} <br>
       Documento :  {!! $requisitoPostulante->postulante->documento!!} <br>
@@ -17,8 +23,21 @@
       <a href="{{ action('PostulanteController@show', $requisitoPostulante->postulante_id ) }}"><p> {!! Form::button('Ver Más!')!!}</a>
 </div>
 
+<!-- Requisitos Id Field -->
+<div class="form-group">
+    {!! Form::label('Requisitoitem_id', 'Requisitos:') !!}
+    <p>
+      Identificador : {!! $requisitoPostulante->requisitoitem_id !!} <br>
+      Descripcion :  {!! $requisitoPostulante->requisitoitem->descripcion !!} - Entrego Requisito: {!! $requisitoPostulante->entregoRequisito !!}<br>
 
-<!-- Requisitoestado Field -->
+      <a href="{{ action('RequisitoItemController@show', $requisitoPostulante->requisitoitem_id ) }}"><p> {!! Form::button('Ver Más!')!!}</a>
+
+    </p>
+</div>
+
+<!-- Aca debemos mostrar los Requisitos con sus estados y poder modificarlos.-->
+
+
 <div class="form-group">
     {!! Form::label('cumpleRequisito', 'cumpleRequisito:') !!}
     <p>{!! $requisitoPostulante->cumpleRequisito !!}</p>
