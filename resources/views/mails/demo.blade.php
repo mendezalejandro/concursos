b46270e0ca0edd622bc594ccd460ac89c8e006f5
Jorge <jorgeoscargamez@gmail.com>
2018-10-09 23:57:40 -0300

    modificando texto de mail

--- resources/views/mails/demo.blade.php
+++ resources/views/mails/demo.blade.php
@@ -1,21 +1,23 @@
-Hola <i>{{ $postulante_nombre }}</i>,
+Buenas Estimado <i>{{ $postulante_nombre }}</i>,
 <p>Informe de Concurso</p>
 
-<p><u>Estimado  , se le informa de la sustanciacion del concurso :</u></p>
+<p><u>Se le informa por este medio de la fecha de sustanciacion del concurso al cual esta afectado:</u></p>
 
 <div>
-<p><b>Referencia General:</b>&nbsp;{{ $referenciaGeneral}}</p>
+<p><b>Referencia General del Concurso:</b>&nbsp;{{ $referenciaGeneral}}</p>
 <p><b>Fecha Sustanciacion:</b>&nbsp;{{ $fechaSustanciacion }}</p>
 </div>
 
-<p><u>Values passed by With method:</u></p>
 
 <div>
 <p><b>Estado del Concurso:</b>&nbsp;{{ $estado }}</p>
-<p><b>Usuario:</b>&nbsp;{{ $usuarioSustanciacion }}</p>
+<!--p><b>Usuario:</b>&nbsp;{{ $usuarioSustanciacion }}</p-->
 </div>
 
 Muchas Gracias
-
+<br/>
+Atte .Concursos Docentes
+<br/>
+Universidad Nacional Arturo Jauretche
 <br/>
 <i>{{ $usuarioSustanciacion }}</i>
